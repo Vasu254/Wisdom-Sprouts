7f1a2d29c83fc189109f215a0e3f031b45132d8d
Vasu254 <vasukumartelugu254@gmail.com>
2025-08-31 12:16:49 +0530

    Day 17

---
+++ Day17.java
@@ -0,0 +1,22 @@
+public class Day17 {
+    public static void primeFactorization(int n) {
+        while (n % 2 == 0) {
+            System.out.print(2 + " ");
+            n /= 2;
+        }
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            while (n % i == 0) {
+                System.out.print(i + " ");
+                n /= i;
+            }
+        }
+        if (n > 2) {
+            System.out.print(n);
+        }
+    }
+    public static void main(String[] args) {
+        int number = 18;
+        System.out.print("Prime factorization of " + number + " is: ");
+        primeFactorization(number);
+    }
+}
