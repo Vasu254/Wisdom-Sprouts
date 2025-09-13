7be83b00ff27a394ec9b92a553f2fc728e8b110e
Vasu254 <vasukumartelugu254@gmail.com>
2025-09-13 16:17:59 +0530

    Day30

---
+++ Day30.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Day30 {
+    public static int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }    
+    public static void main(String[] args) {
+        int[] coins1 = {1, 2, 5};
+        int amount1 = 11;
+        System.out.println(coinChange(coins1, amount1));
+    }
+}
